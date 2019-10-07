99543d2d487f288c9bd659551906f058b73bc421
pooja241 <53747254+pooja241@users.noreply.github.com>
2019-10-07 12:12:11 +0530

    Update case.js

--- modules/case.js
+++ modules/case.js
@@ -46,8 +46,39 @@ exports.execute = (req, res) => {
                     "name": "Assign Owner",
                     "text": "Assign Owner",
                     "type": "button",
+                      "url":"https://poojahqg-dev-ed.my.salesforce.com/5000o00002etFxpAAE";
                     "value": "Assign owner"
-                }
+                },
+                        {
+  "type": "interactive_message",
+  "actions": [
+    {
+      "name": "recommend",
+      "value": "recommend",
+      "type": "button"
+    }
+  ],
+  "callback_id": "comic_1234_xyz",
+  "team": {
+    "id": "T47563693",
+    "domain": "watermelonsugar"
+  },
+  "channel": {
+    "id": "C065W1189",
+    "name": "forgotten-works"
+  },
+  "user": {
+    "id": "U045VRZFT",
+    "name": "brautigan"
+  },
+  "action_ts": "1458170917.164398",
+ // "message_ts": "1458170866.000004",
+  //"attachment_id": "1",
+  //"token": "xAB3yVzGS4BQ3O9FACTa8Ho4",
+  //"original_message": {"text":"New comic book alert!","attachments":[{"title":"The Further Adventures of Slackbot","fields":[{"title":"Volume","value":"1","short":true},{"title":"Issue","value":"3","short":true}],"author_name":"Stanford S. Strickland","author_icon":"https://api.slack.comhttps://a.slack-edge.com/80588/img/api/homepage_custom_integrations-2x.png","image_url":"http://i.imgur.com},
+  "response_url": "https://hooks.slack.com/services/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",
+  
+}
                     
                 ]
         }   
