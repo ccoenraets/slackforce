6d581269d355939edb3bc13b6dce070ceb874ca0
pooja241 <53747254+pooja241@users.noreply.github.com>
2019-10-09 19:34:12 +0530

    Update case.js

--- modules/case.js
+++ modules/case.js
@@ -1,4 +1,4 @@
-"use strict";
+use strict";
 
 let auth = require("./slack-salesforce-auth"),
     force = require("./force"),
@@ -10,16 +10,13 @@ exports.execute = (req, res) => {
         res.send("Invalid token");
         return;
     }
- let slackUserId = req.body.user_id,
-        oauthObj = auth.getOAuthObject(slackUserId),
-        q = "SELECT Id, Name FROM  users LIMIT 5";
 
     let slackUserId = req.body.user_id,
         oauthObj = auth.getOAuthObject(slackUserId),
         params = req.body.text.split(":"),
         subject = params[0],
         description = params[1];
-    
+
     force.create(oauthObj, "Case",
         {
             subject: subject,
@@ -28,20 +25,21 @@ exports.execute = (req, res) => {
             status: "New"
         })
         .then(data => {
-           fields.push({title: "Subject", value: subject, short:false});
+            let fields = [];
+            fields.push({title: "Subject", value: subject, short:false});
             fields.push({title: "Description", value: description, short:false});
             fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + data.id, short:false});
             let message = {
                 text: "A new case has been created:",
                 attachments: [
-                     attachments: [
                     {color: "#F2CF5B", fields: fields}
                 ]
-           res.json(message);
+            };
+            res.json(message);
         })
         .catch((error) => {
             if (error.code == 401) {
-                res.send(`Visit this URL to login to Salesforce: https://${req.hostname}/login/`+ slackUserId);
+                res.send(`Visit this URL to login to Salesforce: https://${req.hostname}/login/` + slackUserId);
 
             } else {
                 res.send("An error as occurred");
