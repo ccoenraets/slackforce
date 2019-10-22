80b7c43468110d07b11c1764b385d792dbcec7c4
pooja241 <53747254+pooja241@users.noreply.github.com>
2019-10-22 08:54:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@
 let express = require('express'),
     bodyParser = require('body-parser'),
     auth = require('./modules/slack-salesforce-auth'),
-    contact = require('./modules/contact'),
-    account = require('./modules/account'),
-    opportunity = require('./modules/opportunity'),
+    //contact = require('./modules/contact'),
+  //  account = require('./modules/account'),
+   // opportunity = require('./modules/opportunity'),
     _case = require('./modules/case'),
-    whoami = require('./modules/whoami'),
+    //whoami = require('./modules/whoami'),
     actions = require('./modules/actions'),
     app = express();
 
@@ -21,11 +21,11 @@ app.use('/', express.static(__dirname + '/www')); // serving company logos after
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/actions', actions.handle);
-app.post('/pipeline', opportunity.execute);
-app.post('/contact', contact.execute);
-app.post('/account', account.execute);
+//app.post('/pipeline', opportunity.execute);
+//app.post('/contact', contact.execute);
+//app.post('/account', account.execute);
 app.post('/case', _case.execute);
-app.post('/whoami', whoami.execute);
+//app.post('/whoami', whoami.execute);
 app.post('/login', auth.loginLink);
 app.post('/logout', auth.logout);
 app.get('/login/:slackUserId', auth.oauthLogin);
